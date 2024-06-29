9c2ad5b7ba2335138e80e114cf790190ccdd1850
Adam Brodzinski <adam.brodzinski@guild.com>
2024-06-28 19:35:16 -0500

    update neovim

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,8 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = { enabled = false },
+
       -- manually setup lspconfig for languages that prefer to use the
       -- lsp installed for the current language version instead of Mason
       servers = {
