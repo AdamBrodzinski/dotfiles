a6cbedcfa93e91bf9086feffa71bbaab7c09f59c
Adam Brodzinski <adambrodzinski@gmail.com>
2024-08-21 21:41:11 -0500

    format eslint + prettier

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -3,11 +3,18 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
-
-      -- manually setup lspconfig for languages that prefer to use the
-      -- lsp installed for the current language version instead of Mason
-      servers = {
-        -- ocamllsp = {},
+      servers = { eslint = {} },
+      -- https://www.lazyvim.org/configuration/recipes#use-eslint-for-fix-on-save-and-prettier-for-formatting
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
       },
     },
   },
