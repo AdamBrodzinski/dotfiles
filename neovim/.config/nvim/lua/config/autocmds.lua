54d061b29c8ef84b2b4dd82099c7593d7b66353c
Adam Brodzinski <adambrodzinski@gmail.com>
2025-06-29 13:04:49 -0500

    remove lazyvim config

--- neovim/.config/nvim/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Autocmds are automatically loaded on the VeryLazy event
-
--- disable annoying markdown hidden chars
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.cmd("verbose set conceallevel=0")
-  end,
-})
