515e06dd35da3ce54fc09623251ebbd96937bfd6
Adam Brodzinski <adambrodzinski@gmail.com>
2025-07-17 02:27:47 +0000

    Update themes

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -834,13 +834,14 @@ require("lazy").setup({
 		},
 	},
 
-	{ -- You can easily change to a different colorscheme.
+	{
+		-- You can easily change to a different colorscheme.
 		-- Change the name of the colorscheme plugin below, and then
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"folke/tokyonight.nvim",
-		-- priority = 1000, -- Make sure to load this before all the other start plugins.
+		priority = 1000,
 		config = function()
 			---@diagnostic disable-next-line: missing-fields
 			require("tokyonight").setup({
@@ -848,19 +849,18 @@ require("lazy").setup({
 					comments = { italic = false }, -- Disable italics in comments
 				},
 			})
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("evergarden-winter")
 		end,
 	},
 
-	{
-		"thesimonho/kanagawa-paper.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
+	{ "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000 },
+
+	{ "thesimonho/kanagawa-paper.nvim", lazy = false, priority = 1000 },
+
+	{ "vague2k/vague.nvim", lazy = false, priority = 1000 },
+
+	{ "webhooked/kanso.nvim", lazy = false, priority = 1000 },
+
+	{ "kamwitsta/vinyl.nvim" },
 
 	{
 		"everviolet/nvim",
@@ -1064,6 +1064,8 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.cmd("verbose set conceallevel=0")
 	end,
 })
+
+vim.cmd("colorscheme kanso")
 --
 -- -- The line beneath this is called `modeline`. See `:help modeline`
 -- -- vim: ts=2 sts=2 sw=2 et
