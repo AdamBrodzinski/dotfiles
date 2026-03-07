5389701d884c5ecd6e938962398532f5695a6a5b
Adam Brodzinski <adambrodzinski@gmail.com>
2026-03-07 15:49:21 -0600

    update-vim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1007,9 +1007,68 @@ require("lazy").setup({
 		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
 
-	-- add config for harpoon
-	-- 				"<leader>hh" to list files
-	-- 				"<leader>hm" to add file
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+
+			harpoon:setup({
+				settings = {
+					save_on_toggle = true,
+				},
+			})
+
+			-- Telescope integration
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
+			-- List harpooned files with telescope
+			vim.keymap.set("n", "<leader>hh", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+				-- toggle_telescope(harpoon:list())
+			end, { desc = "[H]arpoon list files" })
+
+			-- Add current file to harpoon
+			vim.keymap.set("n", "<leader>hm", function()
+				harpoon:list():add()
+			end, { desc = "[H]arpoon [M]ark file" })
+
+			-- Quick select files 1-5
+			vim.keymap.set("n", "<leader>1", function()
+				harpoon:list():select(1)
+			end, { desc = "Harpoon file 1" })
+			vim.keymap.set("n", "<leader>2", function()
+				harpoon:list():select(2)
+			end, { desc = "Harpoon file 2" })
+			vim.keymap.set("n", "<leader>3", function()
+				harpoon:list():select(3)
+			end, { desc = "Harpoon file 3" })
+			vim.keymap.set("n", "<leader>4", function()
+				harpoon:list():select(4)
+			end, { desc = "Harpoon file 4" })
+			vim.keymap.set("n", "<leader>5", function()
+				harpoon:list():select(5)
+			end, { desc = "Harpoon file 5" })
+		end,
+	},
 
 	{ -- Zellij
 		"https://git.sr.ht/~swaits/zellij-nav.nvim",
